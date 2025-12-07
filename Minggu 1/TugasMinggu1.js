06e5f9e1eccd95e9852fad9f3b52607a27bcb635
LinduAjiTrisnadi <linduaji2209@gmail.com>
2025-12-07 17:26:26 +0700

    feat: money logic

--- Minggu 1/TugasMinggu1.js
+++ Minggu 1/TugasMinggu1.js
@@ -2,6 +2,12 @@ const input = require('prompt-sync')({ sigint: true})
 
 const nama = input("Masukan nama: ")
 const umur = Number(input("Masukan Umur: "))
+
+
 if (umur < 21){
-    console.log("Tolak")
+    console.log("Maaf",nama, ", umur kamu kurang", 21 - umur, "tahun lagi")
+}else if (umur >= 21){
+    const uang = Number(input("Masukan Uang: "))
+}else if (uang < 500000){
+    console.log("Maaf", nama, ", uang kamu cuma", uang, "datang lagi lain kali")
 }
